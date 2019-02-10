66d5d920f22a78c14c99c592ea3a3345c077d33d
juan andres andrango <andrango.juanandres@gmail.com>
2019-02-10 14:29:44 -0500

    fix import dj_database_url

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import dj-database-url # add this to the top of your file
+import dj_database_url  # add this to the top of your file
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
